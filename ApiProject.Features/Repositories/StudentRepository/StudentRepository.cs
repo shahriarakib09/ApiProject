2bace67205115853e6f7e2c47ddeb4fe31262cd0
shahriarakib09 <shahriarakib09@gmail.com>
2023-09-21 12:48:09 +0600

    Add project files.

---
+++ ApiProject.Features/Repositories/StudentRepository/StudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Features.Repositories.StudentRepository
+{
+    internal class StudentRepository
+    {
+    }
+}
---
+++ ApiProject.Views/StudentView/StudentView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Views.StudentView
+{
+    internal class StudentView
+    {
+        public Int32 Id { get; set; }
+
+        public string StudentName { get; set; }
+
+        public string Email { get; set; }
+
+        public string StudentRoll { get; set;}
+
+        public string StudentAddress { get; set;}
+
+
+    }
+}
